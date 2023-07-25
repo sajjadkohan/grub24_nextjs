5cfb65d0f197f6577b3a8ee968c9b5b9bffdde6c
sajjadkohan <sajjad.k.9093@gmail.com>
2023-07-25 15:02:38 +0330

    changeOrderType Component

--- src/components/store/ChangeOrderType/ChangeOrderType.js
+++ src/components/store/ChangeOrderType/ChangeOrderType.js
@@ -1,11 +1,49 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import styles from '@/components/store/MerchantDetail.module.css';
 
 const ChangeOrderType = () => {
+
+  const [activeType,setActiveType] = useState(false);
+  const [orderType,setOrderType] = useState('delivery');
+
+  const activeHandler = () => {
+    setActiveType(!activeType);
+    if(confirm(`Warning, certain deals and items are only available for delivery / collection.
+    Changing your order type maybe reset your order.
+    Are you sure you want to change your order type?`)){
+      setOrderType(orderType==='delivery'?'collection' : 'delivery')
+    }
+  }
+
   return (
-    <div className={styles.changeOrderType}>
-      ChangeOrderType
+
+    <div className={`${styles.changeOrderType} dFlexProMax`}>
+
+      <div onClick={activeHandler} className={orderType==='delivery'? `${styles.delivery} ${styles.parentType} ${styles.active}` : `${styles.delivery} ${styles.parentType}` }>
+        <span className='roboto400'>Delivery</span>
+        <p className='roboto700'>
+          Starts From
+          <br/>
+          12:00 PM
+        </p>
+      </div>
+
+      <div style={orderType==='delivery'? {transform : 'translateX(0%)'} : {transform : 'translateX(100%)'}} className={styles.activeTag}>
+
+      </div>
+
+      <div onClick={activeHandler} className={orderType==='delivery'? `${styles.collection} ${styles.parentType}` : `${styles.delivery} ${styles.parentType}  ${styles.active}`}>
+        <span className='roboto400'>Collection</span>
+        <p className='roboto700'>
+          Starts From
+          <br/>
+          12:00 PM
+        </p>
+      </div>
+
     </div>
+    
   )
 }
 
